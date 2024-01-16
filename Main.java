0a6228d3a760dae5ce2d8228eade0d550a07c3ff
Mathis Le balch <69898603+RIKIKIRITO@users.noreply.github.com>
2024-01-16 20:25:56 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    /**
+     * Action : lance une partie de jeu "E3Cète"
+     */
+    public static void main(String[]args) {
+         Jeu jeu = new Jeu();
+         jeu.jouer();
+
+    }
+}
---
+++ Texture.java
@@ -0,0 +1,20 @@
+public enum Texture {
+    Cuir ("C"),
+    Poin ("PO"),
+    Bois ("B"),
+    Rayé ("R");
+
+    private String abreviation ;
+    private Texture(String a) {
+        this.abreviation = a ;
+    }
+    public String getAbreviation() {
+        return this.abreviation ;
+    }
+}
+
+/**
+ * Représente la texture d'une Carte : pleine , à pois...
+ */
+
+
